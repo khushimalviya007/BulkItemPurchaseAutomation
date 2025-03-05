279b9f04b2a7e63b888d928e0e13cfdde67ec620
khushimalviya007 <khushimalviya2018@gmail.com>
2025-03-05 16:15:49 +0530

    First trial commit

---
+++ src/main/java/com/OCR/BulkItemPurchaseAutomation/BulkItemPurchaseAutomationApplication.java
@@ -0,0 +1,15 @@
+package com.OCR.BulkItemPurchaseAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulkItemPurchaseAutomationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(BulkItemPurchaseAutomationApplication.class, args);
+		System.out.println("Application Starteddddd");
+	}
+
+}
---
+++ src/test/java/com/OCR/BulkItemPurchaseAutomation/BulkItemPurchaseAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OCR.BulkItemPurchaseAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulkItemPurchaseAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
